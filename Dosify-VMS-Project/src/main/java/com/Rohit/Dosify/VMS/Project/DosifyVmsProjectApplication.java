ad9d46b8bcbdce1da2f0ab190b7df4e2a959465b
rohit <rohitme0091@gmail.com>
2023-05-14 18:14:20 +0530

    first commit

---
+++ Dosify-VMS-Project/src/main/java/com/Rohit/Dosify/VMS/Project/DosifyVmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Rohit.Dosify.VMS.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosifyVmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosifyVmsProjectApplication.class, args);
+	}
+
+}
---
+++ Dosify-VMS-Project/src/test/java/com/Rohit/Dosify/VMS/Project/DosifyVmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rohit.Dosify.VMS.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosifyVmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
